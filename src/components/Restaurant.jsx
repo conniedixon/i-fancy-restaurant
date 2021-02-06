d330f409d934dd08be82291c0024a0ffefff8eab
connie <conniedixon106@gmail.com>
2021-02-06 23:44:07 +0000

    added emoji img tags for each cuisine

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from "react";
 
+import Argentinian from "../assets/Argentinian.png";
+import Asian from "../assets/Asian.png";
+import Balti from "../assets/Balti.png";
+import Bar from "../assets/Bar.png";
+import British from "../assets/British.png";
+import Cafe from "../assets/Cafe.png";
+import Indian from "../assets/Indian.png";
+import International from "../assets/International.png";
+import Pakistani from "../assets/Pakistani.png";
+import Pub from "../assets/Pub.png";
+import Steakhouse from "../assets/Steakhouse.png";
+import Vietnamese from "../assets/Vietnamese.png";
+
 const Restaurant = ({ restaurant, selectedCuisines }) => {
   const { name, address, cuisine, rating } = restaurant;
   const [show, setShowing] = useState(false);
@@ -18,7 +31,36 @@ const Restaurant = ({ restaurant, selectedCuisines }) => {
       <div>
         <h3>{name}</h3>
         {cuisine.map((cuisine) => (
-          <h6>{cuisine}</h6>
+          <div>
+            <img
+              src={
+                cuisine === "Argentinian"
+                  ? Argentinian
+                  : cuisine === "Asian"
+                  ? Asian
+                  : cuisine === "Balti"
+                  ? Balti
+                  : cuisine === "Bar"
+                  ? Bar
+                  : cuisine === "British"
+                  ? British
+                  : cuisine === "Cafe"
+                  ? Cafe
+                  : cuisine === "Indian"
+                  ? Indian
+                  : cuisine === "International"
+                  ? International
+                  : cuisine === "Pakistani"
+                  ? Pakistani
+                  : cuisine === "Pub"
+                  ? Pub
+                  : cuisine === "Steakhouse"
+                  ? Steakhouse
+                  : Vietnamese
+              }
+              alt={cuisine}
+            />
+          </div>
         ))}
         <p>{address}</p>
       </div>
