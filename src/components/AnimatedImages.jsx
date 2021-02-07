ba48661ccad9c02c1895c473f43cd7efb0ae7cc1
connie <conniedixon106@gmail.com>
2021-02-07 16:15:31 +0000

    imported images and set up animation

---
+++ src/components/AnimatedImages.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import ArgentinianImg from "../assets/Argentinian.png";
+import AsianImg from "../assets/Asian.png";
+import BaltiImg from "../assets/Balti.png";
+import BarImg from "../assets/Bar.png";
+import BritishImg from "../assets/British.png";
+import CafeImg from "../assets/Cafe.png";
+import IndianImg from "../assets/Indian.png";
+import InternationalImg from "../assets/International.png";
+import PakistaniImg from "../assets/Pakistani.png";
+import PubImg from "../assets/Pub.png";
+import SteakhouseImg from "../assets/Steakhouse.png";
+import VietnameseImg from "../assets/Vietnamese.png";
+
+const AnimatedImages = () => {
+  return (
+    <div>
+      <img
+        src={ArgentinianImg}
+        alt='Argentinian'
+        className='homescreen-image animate-1'></img>
+      <img
+        src={AsianImg}
+        alt='Asian'
+        className='homescreen-image animate-2'></img>
+      <img
+        src={BaltiImg}
+        alt='Balti'
+        className='homescreen-image animate-3'></img>
+      <img src={BarImg} alt='Bar' className='homescreen-image animate-4'></img>
+      <img
+        src={BritishImg}
+        alt='British'
+        className='homescreen-image animate-5'></img>
+      <img
+        src={CafeImg}
+        alt='Cafe'
+        className='homescreen-image animate-6'></img>
+      <img
+        src={IndianImg}
+        alt='Indian'
+        className='homescreen-image animate-7'></img>
+      <img
+        src={InternationalImg}
+        alt='International'
+        className='homescreen-image animate-8'></img>
+      <img
+        src={PakistaniImg}
+        alt='Pakistani'
+        className='homescreen-image animate-9'></img>
+      <img src={PubImg} alt='Pub' className='homescreen-image animate-10'></img>
+      <img
+        src={SteakhouseImg}
+        alt='Steakhouse'
+        className='homescreen-image animate-11'></img>
+    </div>
+  );
+};
+
+export default AnimatedImages;
