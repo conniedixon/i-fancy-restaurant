2f87be92e12304aa70f7505f24711f373ff27922
connie <conniedixon106@gmail.com>
2021-02-07 22:24:05 +0000

    tidied code

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import TypeWriter from "./TypeWriter";
 import AnimatedImages from "./AnimatedImages";
 
 const Homepage = () => {
   return (
     <div className='container'>
-      {/* <div className='grey-background'></div> */}
       <TypeWriter />
       <AnimatedImages />
     </div>
